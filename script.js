e298a00ea54da542ff1e3044b9a9e97b757d92cb
Vinuth Marasinghe <83106650+Vinuth004@users.noreply.github.com>
2023-05-07 14:38:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -225,11 +225,3 @@ logo.addEventListener('click', () => {
     window.location.href="index.html"
 });
 
-
-document.addEventListener('keypress', function (e) {
-            if (e.keyCode === 13 || e.which === 13) {
-                e.preventDefault();
-                return false;
-            }
-            
-        });
