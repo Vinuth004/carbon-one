81bea13bd8213a1f2b8b15dbfa4bc6764195a314
Vinuth Marasinghe <83106650+Vinuth004@users.noreply.github.com>
2023-05-07 19:28:14 +0530

    Update apikey.php

--- apikey.php
+++ apikey.php
@@ -1,13 +1,4 @@
 <?php
-// Retrieve the environmental variable from Netlify
-$apiKey = getenv('OPENAI_API_KEY');
-
-// Construct a JSON object containing the API key
-$response = array('apiKey' => $apiKey);
-
-// Set the content type header to JSON
 header('Content-Type: application/json');
-
-// Output the JSON object
-echo json_encode($response);
+echo json_encode(array('apiKey' => getenv('OPENAI_API_KEY')));
 ?>
